5497e87fd7ae34490f734409f0abd4a451bd366e
AndreSantos03 <93008364+AndreSantos03@users.noreply.github.com>
2023-03-18 18:42:41 +0000

    git ignore

---
+++ cmake-build-debug/CMakeFiles/ProjetoDA.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjetoDA.dir/projeto/main.cpp.obj"
+  "CMakeFiles/ProjetoDA.dir/projeto/main.cpp.obj.d"
+  "CMakeFiles/ProjetoDA.dir/projeto/src/information_managment/ReadData.cpp.obj"
+  "CMakeFiles/ProjetoDA.dir/projeto/src/information_managment/ReadData.cpp.obj.d"
+  "ProjetoDA.exe"
+  "ProjetoDA.exe.manifest"
+  "ProjetoDA.pdb"
+  "libProjetoDA.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProjetoDA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
