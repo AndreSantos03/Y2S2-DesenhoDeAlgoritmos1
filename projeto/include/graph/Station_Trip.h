e276a2b932be37780889eb2456863158b38cbdac
diogocs18 <diogopinguinhas@hotmail.com>
2023-04-03 15:45:35 +0100

    test

---
+++ projeto/include/graph/Station_Trip.h
@@ -0,0 +1,84 @@
+#ifndef PROJETODA_STATION_TRIP_H
+#define PROJETODA_STATION_TRIP_H
+
+
+#include <iostream>
+#include <list>
+#include <unordered_set>
+#include <vector>
+
+
+using namespace std;
+
+class Trip;
+
+
+class Station{
+public:
+
+    Station();
+
+    void addAdj(const Trip& trip);
+
+    const string &getName() const;
+
+    void setName(const string &name);
+
+    const string &getDistrict() const;
+
+    void setDistrict(const string &district);
+
+    const string &getMunicipality() const;
+
+    void setMunicipality(const string &municipality);
+
+    const string &getTownship() const;
+
+    void setTownship(const string &township);
+
+    const string &getLine() const;
+
+    void setLine(const string &line);
+
+    const vector<Trip> &getAdj() const;
+
+    void setAdj(const vector<Trip> &adj);
+
+    Station(string name, string district, string municipality, string township, string line);
+
+private:
+    string name, district, municipality, township, line;
+
+    vector<Trip> adj;
+
+
+};
+
+
+
+class Trip{
+public:
+
+    Trip(string destination, double capacity, string service);
+
+    const string &getDestination() const;
+
+    void setDestination(const string &destination);
+
+    double getCapacity() const;
+
+    void setCapacity(double capacity);
+
+    const string &getService() const;
+
+    void setService(const string &service);
+
+private:
+    string destination;
+    double capacity;
+    string service;
+};
+
+
+
+#endif //PROJETODA_STATION_TRIP_H
---
+++ projeto/src/graph/Station_Trip.cpp
@@ -0,0 +1,97 @@
+#include "../../include/graph/Station_Trip.h"
+
+
+Station::Station(string name, string district, string municipality,
+                 string township, string line) : name(name), district(district), municipality(municipality), township(township), line(line){}
+
+
+void Station::addAdj(const Trip& trip) {
+    adj.push_back(trip);
+}
+
+const string &Station::getName() const {
+    return name;
+}
+
+void Station::setName(const string &name) {
+    Station::name = name;
+}
+
+const string &Station::getDistrict() const {
+    return district;
+}
+
+void Station::setDistrict(const string &district) {
+    Station::district = district;
+}
+
+const string &Station::getMunicipality() const {
+    return municipality;
+}
+
+void Station::setMunicipality(const string &municipality) {
+    Station::municipality = municipality;
+}
+
+const string &Station::getTownship() const {
+    return township;
+}
+
+void Station::setTownship(const string &township) {
+    Station::township = township;
+}
+
+const string &Station::getLine() const {
+    return line;
+}
+
+void Station::setLine(const string &line) {
+    Station::line = line;
+}
+
+const vector<Trip> &Station::getAdj() const {
+    return adj;
+}
+
+void Station::setAdj(const vector<Trip> &adj) {
+    Station::adj = adj;
+}
+
+Station::Station() = default;
+
+
+
+/*
+
+||||||||||||| TRIP FUNCTIONS |||||||||||||
+
+*/
+
+
+
+Trip::Trip(string destination, double capacity,
+           string service) : destination(destination), capacity(capacity), service(service) {}
+
+const string &Trip::getDestination() const {
+    return destination;
+}
+
+void Trip::setDestination(const string &destination) {
+    Trip::destination = destination;
+}
+
+double Trip::getCapacity() const {
+    return capacity;
+}
+
+void Trip::setCapacity(double capacity) {
+    Trip::capacity = capacity;
+}
+
+const string &Trip::getService() const {
+    return service;
+}
+
+void Trip::setService(const string &service) {
+    Trip::service = service;
+}
